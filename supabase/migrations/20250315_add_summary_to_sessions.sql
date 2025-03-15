c233efa357a0328c0d8614d98ffc57fb1c93d27d
Stuart Mason <stuart.mason@pfizer.com>
2025-03-15 22:15:58 +0000

    remove the fucker

--- supabase/migrations/20250315_add_summary_to_sessions.sql
+++
@@ -1,5 +0,0 @@
--- Add summary column to sessions table
-ALTER TABLE sessions ADD COLUMN summary TEXT;
-
--- Add comment for documentation
-COMMENT ON COLUMN sessions.summary IS 'Summary of what was accomplished in the session'; 
